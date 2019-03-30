c7dce782a4222948e5c827e91ba64b654b44de55
Samuel <samueladebare1010@gmail.com>
2019-03-30 07:28:55 +0300

    Initial commit

---
+++ wordcount/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from.import views
+
+urlpatterns = [
+	path("", views.homepage, name="home"),
+	path("countwords/", views.count, name = "count"),
+	path("about/", views.about, name="about")
+]
+
